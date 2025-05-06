4ba1eaca57f7861feea9d398c0e22092a3f9c3da
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2025-05-06 21:40:30 +0400

    fix confic next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-
-  basePath: "/next-3d-vehicle",
-  assetPrefix: "/next-3d-vehicle",
-
-  experimental: { styledComponents: true },
-};
+const nextConfig = {};
 
 export default nextConfig;
