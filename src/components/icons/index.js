e4615c813c4dc2ac8b05c8ba45a7b8a66dcfc0f9
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2025-05-06 21:09:23 +0400

    fix bug

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,4 +1,4 @@
 export { default as ChasisIcon } from "./ChasisIcon";
-export { default as WheelIcon } from "./wheelIcon";
+export { default as WheelIcon } from "./WheelIcon";
 export { default as EngineIcon } from "./EngineIcon";
 export { default as BatteryIcon } from "./BatteryIcon";
