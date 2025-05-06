c0474df29cd49eec4469c52b70e2e2199fed31d2
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2025-05-06 21:34:07 +0400

    add new config next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+
+  basePath: "/next-3d-vehicle",
+  assetPrefix: "/next-3d-vehicle",
+
+  experimental: { styledComponents: true },
+};
 
 export default nextConfig;
